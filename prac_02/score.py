6a87f93dd8b60f24744cc100a533d029e2ef015d
Ishan Tripathi <tripathiishan098@gmail.com>
2023-07-11 12:00:11 +1000

    Add from prac 1

--- prac_02/score.py
+++ prac_02/score.py
@@ -1,9 +1,29 @@
-score = float(input("Enter score: "))
-if score < 0 or score > 100:
-    print("Invalid score")
-elif score >= 90:
-    print("Excellent")
-elif score >= 50:
-    print("Passable")
-else:
-    print('Bad')
+import random
+
+MIN_SCORE = -10
+MAX_SCORE = 110
+
+
+def main():
+    score: float = 0
+    score = round(float(generate_score()), 2)
+    print(score)
+    print(sort_score(score))
+
+
+def sort_score(a):
+    if a < 0 or a > 100:
+        return "Invalid score"
+    elif a >= 90:
+        return "Excellent"
+    elif a >= 50:
+        return "Passable"
+    else:
+        return 'Bad'
+
+
+def generate_score():
+    return float(random.uniform(MIN_SCORE, MAX_SCORE))
+
+
+main()
--- prac_02/temperature.py
+++ prac_02/temperature.py
@@ -6,20 +6,29 @@ Pseudocode for temperature conversion
 MENU = """C - Convert Celsius to Fahrenheit
 F - Convert Fahrenheit to Celsius
 Q - Quit"""
+
+
+def con_c_f(a):
+    return a * 9.0 / 5 + 32
+
+
+def con_f_c(a):
+    return 5 / 9 * (a - 32)
+
+
 print(MENU)
 choice = input(">>> ").upper()
 while choice != "Q":
     if choice == "C":
         celsius = float(input("Celsius: "))
-        fahrenheit = celsius * 9.0 / 5 + 32
+        fahrenheit = con_c_f(celsius)
         print(f"Result: {fahrenheit:.2f} F")
     elif choice == "F":
-        # TODO: Write this section to convert F to C and display the result
-        # Hint: celsius = 5 / 9 * (fahrenheit - 32)
-        # Remove the "pass" statement when you are done. It's a placeholder.
-        pass
+        fahrenheit = float(input("fahrenheit: "))
+        celsius = con_f_c(fahrenheit)
+        print(f"Result: {celsius:.2f} C")
     else:
         print("Invalid option")
-print(MENU)
-choice = input(">>> ").upper()
+    print(MENU)
+    choice = input(">>> ").upper()
 print("Thank you.")
